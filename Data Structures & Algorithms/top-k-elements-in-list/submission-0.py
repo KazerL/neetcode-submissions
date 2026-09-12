a5714792b7b87f5d5d7fe134adaca55caff0392b
James Doan <jdoan797@gmail.com>
2026-09-12 15:26:07 -0400

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        nums.sort()
+
+        map = {}
+        count = 0
+        finalOutput = []
+
+        # iterate and add values to hash
+        for entry in nums:
+            # frequency logger
+            if entry in map:
+                map[entry] += 1
+            else:
+                map[entry] = 1
+
+        # finds the k most frequent elements
+        for count in range(k):
+            freqKey = max(map, key=map.get)
+            map.pop(freqKey)
+            finalOutput.append(freqKey)
+        
+        return finalOutput
+
+
+
