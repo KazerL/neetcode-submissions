3115eb2e8c0270585abf496b4cd1ed363eae2da3
James Doan <jdoan797@gmail.com>
2026-09-12 21:17:06 -0400

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    alphabet = {}
+    # starts at 'a'
+    charCode = 97
+    
+
+    def encode(self, strs: List[str]) -> str:
+        # corresponds the word to a char
+        for word in strs:
+            self.alphabet[chr(self.charCode)] = word
+            self.charCode += 1
+
+        # join char array to make encoded string
+        encoded_string = "".join(self.alphabet.keys())
+        return encoded_string
+
+    
+
+    def decode(self, s: str) -> List[str]:
+        decoded_string = list(s)
+        decoded_strs = []
+
+        for entry in decoded_string:
+            decoded_strs.append(self.alphabet.get(entry))
+
+        self.alphabet.clear()
+
+        return decoded_strs
+
+
+
+
