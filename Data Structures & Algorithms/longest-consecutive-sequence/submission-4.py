3ef2f6a5abbf08fb995fd54214b03aeab3859824
James Doan <jdoan797@gmail.com>
2026-09-13 22:31:04 -0400

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums.sort()
+        map = {}
+        counter = 1
+        i = 0
+
+        for entry in nums:
+            # check out of bounds
+            if i + 1 >= len(nums):
+                map[i] = counter
+                break
+            # checks for duplicates
+            elif nums[i + 1] - nums[i] == 0:
+                i += 1
+                continue
+            # difference between consecutive next elements
+            elif nums[i + 1] - nums[i] != 1:
+                map[i] = counter
+                counter = 1
+                i += 1
+                continue
+            else:
+                counter += 1
+                i += 1
+
+        finValue = max(map.values(), default = 0)
+
+        return finValue
+
+        
+            
+
